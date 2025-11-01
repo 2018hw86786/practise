ecc3eb8959fb9301b27fad753d70b7ee41f70f9b
Omprakash Prajapati <2018hw86786@wilp.bits-pilani.ac.in>
2025-11-01 18:30:18 +0530

    upsated the gitpush script

--- gitcmd.sh
+++ gitcmd.sh
@@ -5,9 +5,7 @@ git config --global user.email "2018hw86786@wilp.bits-pilani.ac.in"
 git config --global user.name "Omprakash Prajapati"
 git remote add origin https://github.com/2018hw86786/practise.git
 git branch -M main
-git add .
-git commit -m "first commit"
-git pull --rebase origin main || true
+git checkout main
 git push -u origin main
 git push --set-upstream origin main
 git push
