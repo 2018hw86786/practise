22992c4261cb3e6cb36a2dbb1525be6883e99fe8
Omprakash Prajapati <2018hw86786@wilp.bits-pilani.ac.in>
2025-11-01 18:24:18 +0530

    first commit

--- gitcmd.sh
+++ gitcmd.sh
@@ -4,9 +4,9 @@ git init
 git config --global user.email "2018hw86786@wilp.bits-pilani.ac.in"
 git config --global user.name "Omprakash Prajapati"
 git remote add origin https://github.com/2018hw86786/practise.git
-git add .
-git commit -m "first coomit"
 git branch -M main
+git add .
+git commit -m "first commit"
 git pull --rebase origin main || true
 git push -u origin main
 git push --set-upstream origin main
